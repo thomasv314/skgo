2cc46259807f153c14c08bcbafa6c13f87c724f0
Thomas Vendetta <tvendetta@weblinc.com>
2017-01-02 23:34:55 -0500

    ref

--- main.go
+++ main.go
@@ -6,24 +6,19 @@ import (
 )
 
 func main() {
-	client := sidekiq.NewClient("localhost:6379", "wl_ops_app_sidekiq")
+	client := sidekiq.NewClient("localhost:6379", "mynamespace")
+	info, _ := client.Info()
+	processes, _ := client.Processes()
 
-	info, err := client.Info()
+	for i = range processes {
+		process := processes[i]
+		jobs := client.JobsForProcess(process.Name)
 
-	fmt.Println("info", info, err)
-	fmt.Println("\n")
-
-	procs, err := client.Processes()
-
-	for i := range procs {
-		proc := procs[i]
-		jobs := client.JobsForProcess(proc.Name)
 		fmt.Println("Jobs for", proc.Name)
 		for j := range jobs {
 			job := jobs[j]
 			fmt.Println("Job", job.Payload.Id, "running in", job.Queue)
 			fmt.Println("Payload", jobs[j].Payload)
-			fmt.Println("--")
 		}
 	}
 }
